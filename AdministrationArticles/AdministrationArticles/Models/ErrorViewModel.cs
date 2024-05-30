c4085660c103285a49abe9644520495b47e3a61d
mhdbzb <117605651+mhdbzb@users.noreply.github.com>
2024-05-30 14:12:47 +0200

    corrections répertoires

---
+++ AdministrationArticles/AdministrationArticles/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AdministrationArticles.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
