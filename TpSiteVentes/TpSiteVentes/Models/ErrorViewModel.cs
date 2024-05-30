b232c88e59d44afbd9cf4a2184c48caceacf1612
mhdbzb <117605651+mhdbzb@users.noreply.github.com>
2024-05-30 14:00:54 +0200

    création du projet

---
+++ TpSiteVentes/TpSiteVentes/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TpSiteVentes.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
